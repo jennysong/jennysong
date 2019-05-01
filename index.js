d8475a6f0efb017dba4250e7bb032c0855d3d703
Jenny Song <jenny.jieun.song@gmail.com>
2019-04-30 21:23:32 -0700

    image

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HELLO WORLD! :) ')
-})
+
+// app.get('/', (req, res) => {
+//   res.send('HELLO WORLD! :) ')
+// })
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
